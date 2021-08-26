704415f9b86cb754d60b9ee870feb38de4632128
V radiulov <radiulovv.a@gmail.com>
2021-08-26 19:01:27 +1000

    create project + binding

---
+++ app/src/main/java/com/example/contactlist/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.contactlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.contactlist.databinding.MainActivityBinding
+import com.example.contactlist.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: MainActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(binding.container.id, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
